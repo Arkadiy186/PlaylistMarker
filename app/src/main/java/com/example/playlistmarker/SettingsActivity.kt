a124679141ef12da2e738c094e8b54fe1100d21f
Arkadiy <danilov-av2004@mail.ru>
2024-10-26 20:59:17 +0300

    Changed the button, moved iconPadding to dimens, removed startActivity and replaced it with finish, changed the text size and removed the capsize

--- app/src/main/java/com/example/playlistmarker/SettingsActivity.kt
+++ app/src/main/java/com/example/playlistmarker/SettingsActivity.kt
@@ -3,6 +3,7 @@ package com.example.playlistmarker
 import android.content.Intent
 import android.os.Bundle
 import android.widget.Button
+import android.widget.ImageButton
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -14,10 +15,9 @@ class SettingsActivity : AppCompatActivity() {
         enableEdgeToEdge()
         setContentView(R.layout.activity_settings)
 
-        val backToMainButton = findViewById<Button>(R.id.back_to_main)
+        val backToMainButton = findViewById<ImageButton>(R.id.back_to_main)
         backToMainButton.setOnClickListener {
-            val backToMainButtonIntent = Intent(this, MainActivity::class.java)
-            startActivity(backToMainButtonIntent)
+            finish()
         }
     }
 }
